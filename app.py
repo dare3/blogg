7002b42297bec23cb48c29d777ffef9b243d3902
Dare3 <enitan_segun@yahoo.co.uk>
2024-07-24 15:46:36 -0400

    app.py modified with routes

---
+++ app.py
@@ -0,0 +1,80 @@
+"""Blogly application."""
+
+from flask import Flask
+from model import db, connect_db
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = 'secretkey'
+
+
+
+toolbar = DebugToolbarExtension(app)
+
+db = SQLAlchemy(app)
+
+from model import db, User
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
+
+@app.route('/')
+def root():
+    return redirect(url_for('list_users'))
+
+@app.route('/users')
+def list_users():
+    users = User.query.all()
+    return render_template('list.html', users=users)
+
+@app.route('/users/new')
+def new_user():
+    return render_template('new.html')
+
+@app.route('/users/new', methods=['POST'])
+def add_user():
+    first_name = request.form['first_name']
+    last_name = request.form['last_name']
+    image_url = request.form['image_url'] or 'https://via.placeholder.com/150'
+
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_url)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('list_users'))
+
+@app.route('/users/<int:user_id>')
+def show_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('detail.html', user=user)
+
+@app.route('/users/<int:user_id>/edit')
+def edit_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return render_template('edit.html', user=user)
+
+@app.route('/users/<int:user_id>/edit', methods=['POST'])
+def update_user(user_id):
+    user = User.query.get_or_404(user_id)
+    user.first_name = request.form['first_name']
+    user.last_name = request.form['last_name']
+    user.image_url = request.form['image_url']
+
+    db.session.commit()
+    return redirect(url_for('list_users'))
+
+@app.route('/users/<int:user_id>/delete', methods=['POST'])
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return redirect(url_for('list_users'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
