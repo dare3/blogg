77d6e8a1241dd9adc3f28c72c0ec7be6a4ba85d8
Dare3 <enitan_segun@yahoo.co.uk>
2024-07-29 18:07:13 -0400

    modified

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from models import Post, connect_db, db, User
 from flask_debugtoolbar import DebugToolbarExtension
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://blogly'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 app.config['SECRET_KEY'] = 'secretkey'
